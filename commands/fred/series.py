4b7feff6d1c70390452406921cf69c13eff82efe
Adrian Webb <adrian@webb.sh>
2022-11-24 04:43:47 -0500

    Fixing a print statement in the fred series command.

--- commands/fred/series.py
+++ commands/fred/series.py
@@ -4,4 +4,4 @@ from systems.commands.index import Command
 class Series(Command('fred.series')):
 
     def exec(self):
-        print(self.fred.search_by_category(18, order_by = 'popularity', sort_order = 'desc'))
+        self.info(self.fred.search_by_category(18, order_by = 'popularity', sort_order = 'desc'))
