06cd7c9426a646cc40000d7c67b79aa47caab1e1
Adrian Webb <adrian@webb.sh>
2022-11-24 04:15:18 -0500

    Adding get_categories and get_series_categories methods to the FredAPI utility class.

--- utility/fred.py
+++ utility/fred.py
@@ -2,4 +2,24 @@ from fredapi import Fred
 
 
 class FredAPI(Fred):
-    pass
+
+    def get_categories(self, category_id = 0):
+        url = "%s/category/children?category_id=%s" % (self.root_url, category_id)
+        root = self._Fred__fetch_data(url)
+        categories = []
+
+        if root is not None:
+            for child in root:
+                categories.append(child.attrib)
+        return categories
+
+
+    def get_series_categories(self, series_id):
+        url = "%s/series/categories?series_id=%s" % (self.root_url, series_id)
+        root = self._Fred__fetch_data(url)
+        categories = []
+
+        if root is not None:
+            for child in root:
+                categories.append(child.attrib)
+        return categories
