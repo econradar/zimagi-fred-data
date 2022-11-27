5881d5515fcdc39632e02eef9ced496331f8c33a
Adrian Webb <adrian@webb.sh>
2022-11-27 06:11:50 -0500

    Moving the get_columns and get_values methods to the fred series plugin mixin and adding a cache to the fred accessor.

--- plugins/mixins/fred_series.py
+++ plugins/mixins/fred_series.py
@@ -8,4 +8,17 @@ class FREDSeriesMixin(ProviderMixin('fred_series')):
 
     @property
     def fred(self):
-        return FredAPI(self, settings.FRED_API_KEY)
+        if not getattr(self, '_fred_api', None):
+            self._fred_api = FredAPI(self, settings.FRED_API_KEY)
+        return self._fred_api
+
+
+    def get_columns(self, *columns):
+        return [ 'source', 'series_id', *columns ]
+
+    def get_values(self, data, *columns):
+        return [
+            settings.FRED_SOURCE_NAME,
+            self.field_series_id,
+            *[ data.get(column, None) for column in columns ]
+        ]
