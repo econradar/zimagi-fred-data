233c75beb9d1e2f2b59b59d4417f7adc5ca163b2
Adrian Webb <adrian@webb.sh>
2022-11-27 12:18:22 -0500

    Fixing load_items method call in the FRED revisions source plugin provider.

--- plugins/source/fred_revisions.py
+++ plugins/source/fred_revisions.py
@@ -6,7 +6,7 @@ class Provider(BaseProvider('source', 'fred_revisions')):
     def item_columns(self):
         return self.get_columns('date', 'target_date', 'value')
 
-    def load_fred_series(self, indicator_id, start_date, end_date):
+    def load_items(self, context):
         yield from self.fred.get_data_revisions(
             self.field_series_id,
             observation_start = self.get_start_date()
