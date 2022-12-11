f64314e314bc376cf9ebed1033adcf57242bb581
Adrian Webb <adrian@webb.sh>
2022-12-10 22:29:41 -0500

    Adding a get_series_updates method and internal update tracking class in the FRED data plugin mixin implementation.

--- plugins/mixins/fred_data.py
+++ plugins/mixins/fred_data.py
@@ -3,6 +3,17 @@ from django.conf import settings
 from systems.plugins.index import ProviderMixin
 
 
+class FREDUpdates(object):
+
+    data = {}
+
+    @classmethod
+    def get_data(cls, fred, start_time):
+        if start_time not in cls.data:
+            cls.data[start_time] = list(fred.get_updated_series(start_time))
+        return cls.data[start_time]
+
+
 class FREDDataMixin(ProviderMixin('fred_data')):
 
     def get_start_date(self):
@@ -10,3 +21,17 @@ class FREDDataMixin(ProviderMixin('fred_data')):
         if not isinstance(date, str):
             date = date.strftime('%Y-%m-%d')
         return date
+
+
+    def get_series_updates(self, start_time):
+        if not isinstance(start_time, str):
+            start_time = start_time.strftime('%Y%m%d%H%M')
+
+        data = None
+
+        def get_updates():
+            nonlocal data
+            data = FREDUpdates.get_data(self.fred, start_time)
+
+        self.command.run_exclusive("fred_plugin_series_updates_{}".format(start_time), get_updates)
+        return data
